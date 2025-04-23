c86109ff019c19c51edc6cfb4d3293b1f738eb93
Gabrielle Kraft <kraftgabrielle@gmail.com>
2025-04-23 08:37:46 -0400

    Numbered functions; added work plan doc

--- R/function_code.R
+++ R/function_code.R
@@ -1,5 +1,5 @@
-# * plot_distributions(df) – Auto-generates histograms/bar plots for all variables
-# * standardize_dates() – Converts different date formats to YYYY-MM-DD
-# * analog_time()— Coverts clock time to military time
-# * detect_outliers() – Flags extreme values using IQR or Z-scores
-# * highlight_point()— Makes a specified point red/labeled on a scatterplot
+# 1 * plot_distributions(df) – Auto-generates histograms/bar plots for all variables
+# 2 * standardize_dates() – Converts different date formats to YYYY-MM-DD
+# 3 * analog_time()— Coverts clock time to military time
+# 4 * detect_outliers() – Flags extreme values using IQR or Z-scores
+# 5 * highlight_point()— Makes a specified point red/labeled on a scatterplot
