0a1a03f788a75e45b5fcbee8ff0df5cb6c6d9e35
Gabrielle Kraft <kraftgabrielle@gmail.com>
2025-04-23 08:34:35 -0400

    functions to write

---
+++ R/function_code.R
@@ -0,0 +1,5 @@
+# * plot_distributions(df) – Auto-generates histograms/bar plots for all variables
+# * standardize_dates() – Converts different date formats to YYYY-MM-DD
+# * analog_time()— Coverts clock time to military time
+# * detect_outliers() – Flags extreme values using IQR or Z-scores
+# * highlight_point()— Makes a specified point red/labeled on a scatterplot
