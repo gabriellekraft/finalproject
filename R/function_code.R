fc0cd5e4af4e861603a3fe0eff9a8ff9ca219ee0
Gabrielle Kraft <kraftgabrielle@gmail.com>
2025-04-30 11:17:41 -0400

    prelim function

---
+++ R/function_code.R
@@ -0,0 +1,29 @@
+
+sds_packages <- function(sds_packages, install_missing = TRUE, quiet = FALSE) {
+  loaded <- setNames(logical(length(sds_packages)), sds_packages)
+
+  for (pkg in sds_packages) {
+    if (!require(pkg, character.only = TRUE, quietly = TRUE)) {
+      if (install_missing) {
+        if (!quiet) message("Installing package:", pkg)
+        tryCatch({
+          utils::install.packages(pkg, quiet = TRUE)
+          if (require(pkg, character.only = TRUE, quietly = TRUE)) {
+            loded[pkg] <- TRUE
+            if (!quiet) message("Successfully installed and loaded:", pkg)
+          } else {
+            if (!quiet) warning("Installation succeeded but loading failed for:", pkg)
+          }
+        }, error = function(e) {
+          if (!quiet) warning("Failed to install", pkg, ": ", e$message)
+        })
+      } else {
+        if (!quiet) warning("Package not available:", pkg)
+      }
+    } else {
+      loaded[pkg] <- TRUE
+      if (!quiet) message("Package loaded:u", pkg)
+    }
+  }
+
+}
